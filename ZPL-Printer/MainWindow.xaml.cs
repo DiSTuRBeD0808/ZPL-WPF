51ea1542eaca83c53b9188367804b9f5570ddb3e
DiSTuRBeD0808 <shinigami0808@gmail.com>
2019-08-27 16:24:10 +0200

    Added Pick labels function
    Added function and logic to enable printing of pick labels

--- ZPL-Printer/MainWindow.xaml.cs
+++ ZPL-Printer/MainWindow.xaml.cs
@@ -34,6 +34,20 @@ namespace ZPL_Printer
       }
     }
 
+    private void NSIsSerialized_Checked(object sender, RoutedEventArgs e)
+    {
+      if (NSisSerialized.IsChecked == true)
+      {
+        NSserialNoField.IsEnabled = true;
+        NSserialNoField.IsEnabled = true;
+      }
+      else
+      {
+        NSserialNoField.IsEnabled = false;
+        NSserialNoField.IsEnabled = false;
+      }
+    }
+
     public void MatDoc_TextChanged(object sender, TextChangedEventArgs e)
     {
 
@@ -130,15 +144,7 @@ namespace ZPL_Printer
         }
       }
     }
-/*
-    public void ipAddressField_TextChanged(object sender, TextChangedEventArgs e)
-    {
-      string ipAddress = ipAddressField.Text;
-      NSipAddressField.Text = ipAddress;
-      string nsIpAddress = NSipAddressField.Text;
-      ipAddressField.Text = nsIpAddress;
-    }
-*/
+
     public void NSPrint_Click(object sender, RoutedEventArgs e)
     {
       {
@@ -172,7 +178,7 @@ namespace ZPL_Printer
 
           string NSZPLString =
 
-          ("^XA^FS^FT220,70^A0N,70,70^FH\\^FN1^FD" + NSmatNo + "^FS^FT26,110^A0N,43,44^FH\\^FD" + NSmatDesc + "^FS^MMT^LH0,0^CI0^BY110,110^FT26,237^BXN,5,200,22,22,1^FH\\^FN5^FD" + NSbarcode + "^FS^FT274,150^A0N,43,44^FH\\^FDNonStock^FS^FT500,150^A0N,43,44^FH\\^FN3^FDQuantity: " + NSqty + "^FS^FT258,200^A0N,43,44^FH\\^FDDoc:^FS^FT350,200^A0N,43,44^FH\\^FN8^FDNS" + NSmatDoc + NSlineItem + "^FS^FT256,250^A0N,43,44^FH\\^FDS.Nr:^FS^FT350,250^A0N,43,44^FH\\^FN2^FD" + NSserialNoField.Text + "^FS");
+          ("^XA^FS^FT220,70^A0N,70,70^FH\\^FN1^FD" + NSmatNo + "^FS^FT26,110^A0N,43,44^FH\\^FD" + NSmatDesc + "^FS^MMT^LH0,0^CI0^BY110,110^FT26,237^BXN,5,200,22,22,1^FH\\^FN5^FD" + NSbarcode + "^FS^FT274,150^A0N,43,44^FH\\^FDNonStock^FS^FT256,290^A0N,43,44^FH\\^FN3^FDQuantity: " + NSqty + "^FS^FT258,200^A0N,43,44^FH\\^FDDoc:^FS^FT350,200^A0N,43,44^FH\\^FN8^FDNS" + NSmatDoc + "-" + NSlineItem + "^FS^FT256,250^A0N,43,44^FH\\^FDS.Nr:^FS^FT350,250^A0N,43,44^FH\\^FN2^FD" + NSserialNoField.Text + "^FS");
           try
 
           {
@@ -190,6 +196,104 @@ namespace ZPL_Printer
         new System.IO.StreamWriter(client.GetStream());
 
             writer.Write(NSZPLString);
+
+            writer.Flush();
+
+            // Close Connection
+
+            writer.Close();
+
+            client.Close();
+
+          }
+
+          catch (Exception ex)
+
+          {
+
+            // Catch Exception
+
+          }
+        }
+      }
+    }
+
+    private void PresNoField_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void PlineItemField_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void PmatNoField_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void PmatDescField_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void PqtyField_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void Pprint_Click(object sender, RoutedEventArgs e)
+    {
+      {
+        string PreservationNo = PreservationField.Text;
+        string PlineItem = PlineItemField.Text;
+        string PmatNo = PmatNoField.Text;
+        string PmatDesc = PmatDescField.Text;
+        string Pquantity = PquantityField.Text;
+
+
+        string Pbarcode = "P" + PreservationNo + "-" + PlineItem + "-" + PmatNo;
+
+        MessageBoxResult PlabelResult = MessageBox.Show(Pbarcode);
+
+        print_zpl(Pbarcode);
+
+
+        void print_zpl(string label)
+        {
+          if (string.IsNullOrEmpty(label))
+          {
+            throw new ArgumentException("Please enter valid barcode data.", nameof(label));
+          }
+          // Printer IP Address and communication port
+
+          string ipAddress = ipAddressField.Text;
+
+          int port = 9100;
+
+          // ZPL Command(s)
+
+          string PZPLString =
+
+          ("^XA^FS^FT170,70^A0N,45,45^FH\\^FN1^FD" + PmatNo + "^FS^FT30,130^A0N,35,35^FH\\^FN2^FD" + PmatDesc + "^FS^MMT^LH0,0^CI0^BY110,110^FT46,270^BXN,5,200,22,22,1^FH\\^FN3^FD" + Pbarcode + "^FS^FT335,185^A0N,23,24^FH\\^FDRes:^FS^FT400,185^A0N,25,24^FH\\^FN4^FD" + PreservationNo + "-" + PlineItem + "^FS^FT338,205^A0N,23,24^FH\\^FDQty:^FS^FT400,205^A0N,25,24^FH\\^FN10^FD" + Pquantity + "^FS^XZ");
+          try
+
+          {
+
+            // Open connection
+
+            System.Net.Sockets.TcpClient client = new System.Net.Sockets.TcpClient();
+
+            client.Connect(ipAddress, port);
+
+            // Write ZPL String to connection
+
+            System.IO.StreamWriter writer =
+
+        new System.IO.StreamWriter(client.GetStream());
+
+            writer.Write(PZPLString);
 
             writer.Flush();
 
